45538cd29cb573167ecbc76965c0bcd1744a500f
sruba mohapatra <sruba209239@gmail.com>
2018-01-30 08:20:27 +0530

    2nd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  second: string;
+  first: string;
+  result:string;
+  operation:string;
+
+  constructor(){
+    this.result="0";
+    this.operation="";
+  }
+
+  onClick(e){
+    var number=e.target.innerText.trim();
+    if(parseInt(this.result)==0){
+      this.result=number;
+    }
+    else{
+      // debugger;
+      this.result+=number;
+    }
+
+    if(this.operation==''){
+      this.first=this.result;
+    }
+    else{
+      this.second=this.result;
+    }
+  }
+
+  onSignClick(e){
+    this.operation=e.target.innerText.trim();
+    this.result='';
+  }
+
+  onEqualsClick(e){
+    if(this.operation != ''){
+      this.result='';
+      switch(this.operation){
+        case '+':
+        this.result=(parseInt(this.first)+parseInt(this.second)).toString();
+        break;
+
+        case '-':
+        this.result=(parseInt(this.first)-parseInt(this.second)).toString();
+        break;
+
+        case 'X':
+        this.result=(parseInt(this.first)*parseInt(this.second)).toString();
+        break;
+
+        case '/':
+        this.result=(parseInt(this.first)/parseInt(this.second)).toString();
+        break;
+      }
+    }
+    
+  }
 }
